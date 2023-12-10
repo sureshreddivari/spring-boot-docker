f0e3cfb9b8b9e29adecbf77032a4358329839d0a
suresh-reddivai <sureshreddy.roddam@gmail.com>
2023-12-10 17:27:48 +0530

    spring security added partially

--- src/main/java/com/docker/DockerTest/DockerTestApplication.java
+++ src/main/java/com/docker/DockerTest/DockerTestApplication.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.docker")
 public class DockerTestApplication {
-
 	public static void main(String[] args) {
 		System.out.println("Starting point");
 		SpringApplication.run(DockerTestApplication.class, args);
---
+++ src/main/java/com/docker/DockerTest/User.java
@@ -0,0 +1,20 @@
+package com.docker.DockerTest;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @NotBlank
+    private String userName;
+    @NotBlank
+    private String password;
+    @Email
+    private String email;
+}
